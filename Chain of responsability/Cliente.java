8cf4e472e568041d4b839e3f9a03bfd6ede7500f
JordanUtrera <78768750+JordanUtrera@users.noreply.github.com>
2021-05-18 13:06:40 -0500

    Initial commit

---
+++ Chain of responsability/Cliente.java
@@ -0,0 +1,16 @@
+public class Cliente {
+    public static void main(String argv[]) {
+      Unidad smith  = new Coronel("Smith", null);
+      Unidad truman = new Coronel("Truman", "Tomar posición enemiga");
+      Unidad ryan   = new Soldado("Ryan");
+      Unidad rambo  = new Soldado("Rambo");
+      
+      System.out.println(rambo.orden());    // rambo ->
+
+      rambo.establecerMando(truman);
+      System.out.println(rambo.orden());    // rambo -> truman
+
+      ryan.establecerMando(rambo);
+      System.out.println(ryan.orden());     // ryan -> rambo -> truman
+    }
+}
---
+++ Chain of responsability/Soldado.java
@@ -0,0 +1,9 @@
+public class Soldado extends Unidad {
+    // el constructor sólo tiene que inicializar la parte correspondiente a la superclase
+    public Soldado(String nombre) {
+        super(nombre);
+    }
+    public String toString() {
+        return ("Soldado " + super.toString()); 
+    }
+}
